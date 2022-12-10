2ec74dddd08d9f818614d29c8d13dd467a8ae697
Leo_Amv <leonidamvrosov@yandex.ru>
2022-12-10 17:42:07 +0500

    Homework completed!

--- src/main/java/ru/gb/oseminar5/Main.java
+++ src/main/java/ru/gb/oseminar5/Main.java
@@ -1,16 +1,13 @@
 package ru.gb.oseminar5;
 
 import ru.gb.oseminar5.controller.UserController;
-import ru.gb.oseminar5.model.FileOperation;
-import ru.gb.oseminar5.model.FileOperationImpl;
-import ru.gb.oseminar5.model.Repository;
-import ru.gb.oseminar5.model.RepositoryFile;
+import ru.gb.oseminar5.model.*;
 import ru.gb.oseminar5.view.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
-        FileOperation fileOperation = new FileOperationImpl("users.txt");
-        Repository repository = new RepositoryFile(fileOperation);
+        FileOperation fileOperation = new NewFileOperation("users2.txt");
+        Repository repository = new NewRepositoryFile(fileOperation);
         UserController controller = new UserController(repository);
         ViewUser view = new ViewUser(controller);
         view.run();
---
+++ src/main/java/ru/gb/oseminar5/model/NewFileOperation.java
@@ -0,0 +1,57 @@
+package ru.gb.oseminar5.model;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewFileOperation implements FileOperation{
+    private final String fileName;
+
+    public NewFileOperation(String fileName) {
+        this.fileName = fileName;
+        try (FileWriter writer = new FileWriter(fileName, true)){
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> readAllLines() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            if (line != null){
+                lines.add(line);
+            }
+            while(line!=null){
+                line = reader.readLine();
+                if (line != null&& !line.isEmpty()){
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    @Override
+    public void saveAllLines(List<String> lines) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            for (String line : lines) {
+                writer.write(line+"\n");
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
